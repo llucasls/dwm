007678a4c2bb12b67f07dd513b5dca89c007fd3d
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-06-18 20:47:11 -0300

    apply personal configs
    patches used:
      - systray
      - ru_gaps
      - rotate tags
      - rotate stack
      - rainbowtags
      - adjacenttag

--- config.h
+++ config.h
@@ -11,32 +11,38 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char *fonts[]          = { "Firacode:style=Retina:size=11" };
+static const char dmenufont[]       = "Firacode:style=Retina:size=11";
+static const char col_fg[]       = "#2d80d2";
+static const char sel_fg[]       = "#b4690f";
+static const char col_bg[]       = "#0f0f0f";
+static const char sel_bg[]       = "#0f0f0f";
+static const char col_border[]       = "#7b7b7b";
+static const char sel_border[]       = "#b4690f";
+static const char col_dmenu_bg[]       = "#0f0f0f";
+static const char col_dmenu_fg[]       = "#2d80d2";
+static const char sel_dmenu_bg[]       = "#0f0f0f";
+static const char sel_dmenu_fg[]       = "#b4690f";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_fg, col_bg, col_border },
+	[SchemeSel]  = { sel_fg, sel_bg, sel_border },
 };
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const char *tagsel[][2] = {
-	{ "#ffffff", "#ff0000" },
-	{ "#ffffff", "#ff7f00" },
-	{ "#000000", "#ffff00" },
-	{ "#000000", "#00ff00" },
-	{ "#ffffff", "#0000ff" },
-	{ "#ffffff", "#4b0082" },
-	{ "#ffffff", "#9400d3" },
-	{ "#000000", "#ffffff" },
-	{ "#ffffff", "#000000" },
+	/*    fg         bg    */
+	{ "#b4690f", "#0f0f0f" },
+	{ "#b4690f", "#0f0f0f" },
+	{ "#b4690f", "#0f0f0f" },
+	{ "#b4690f", "#0f0f0f" },
+	{ "#b4690f", "#0f0f0f" },
+	{ "#b4690f", "#0f0f0f" },
+	{ "#b4690f", "#0f0f0f" },
+	{ "#b4690f", "#0f0f0f" },
+	{ "#b4690f", "#0f0f0f" },
 };
 
 static const Rule rules[] = {
@@ -63,7 +69,8 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
+#define AltMask Mod1Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -75,14 +82,27 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb",
+  col_dmenu_bg, "-nf", col_dmenu_fg, "-sb", sel_dmenu_bg, "-sf", sel_dmenu_fg, NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
+static const char *term2cmd[]  = { "kitty", NULL };
+static const char *browsercmd[]  = { "brave", NULL };
+static const char *gamecmd[]  = { "steam", NULL };
+static const char *statcmd[]  = { "alacritty", "-t", "stat", "-e", "radian", "-q" };
+static const char *brcmd[]  = { "br-keys", NULL };
+static const char *offcmd[]  = { "shutdown", "now" };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ NULL,                         XK_Menu,   spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = term2cmd } },
+	{ MODKEY,                       XK_b,      spawn,          {.v = browsercmd } },
+	{ MODKEY,                       XK_g,      spawn,          {.v = gamecmd } },
+	{ MODKEY,                       XK_r,      spawn,          {.v = statcmd } },
+	{ MODKEY,                       XK_c,      spawn,          {.v = brcmd } },
+	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
 	{ MODKEY|ShiftMask,             XK_j,      rotatestack,    {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_k,      rotatestack,    {.i = -1 } },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
@@ -91,9 +111,9 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY,                       XK_x,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
@@ -103,14 +123,14 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY|ControlMask,           XK_comma,  tagmon,         {.i = -1 } },
+	{ MODKEY|ControlMask,           XK_period, tagmon,         {.i = +1 } },
 	{ MODKEY,                       XK_Right,  viewnext,       {0} },
 	{ MODKEY,                       XK_Left,   viewprev,       {0} },
 	{ MODKEY|ShiftMask,             XK_Right,  tagtonext,      {0} },
 	{ MODKEY|ShiftMask,             XK_Left,   tagtoprev,      {0} },
-	{ MODKEY,                       XK_Left,   rotatetags,     {.i = -1 } },
-	{ MODKEY,                       XK_Right,  rotatetags,     {.i = +1 } },
+	{ MODKEY|AltMask,               XK_Left,   rotatetags,     {.i = -1 } },
+	{ MODKEY|AltMask,               XK_Right,  rotatetags,     {.i = +1 } },
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -5 } },
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +5 } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
@@ -124,6 +144,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|AltMask,               XK_Escape, spawn,          {.v = offcmd } },
 };
 
 /* button definitions */
